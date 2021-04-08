d3a48db8fa9fac1ba5f1134fc9c7ce920e643c69
Dave Spina <dspina79@gmail.com>
2021-04-07 21:02:38 -0400

    Basic game setup

--- Arithmetica/Arithmetica/main.swift
+++ Arithmetica/Arithmetica/main.swift
@@ -7,5 +7,67 @@
 
 import Foundation
 
-print("Hello, World!")
+enum QuestionType: CaseIterable {
+    case add
+    case subtract
+    case multiply
+}
 
+class GameContext {
+    var questionNumber = 1
+    var score = 0
+    
+    func start() {
+        print("Welcome to Arithmetica!\n")
+        
+        repeat {
+            let left = Int.random(in: 1...12)
+            let right = Int.random(in: 1...12)
+            let questionType = QuestionType.allCases.randomElement()!
+            
+            let question: String
+            let answer: Int
+            
+            switch questionType {
+            case .add:
+                question = "What is \(left) + \(right)?"
+            case .subtract:
+                question = "What is \(left) - \(right)?"
+            case .multiply:
+                question = "What is \(left) x \(right)?"
+            }
+            
+            // two switch cases for now...
+            switch questionType {
+            case .add:
+                answer = left + right
+            case .subtract:
+                answer = left - right
+            case .multiply:
+                answer = left * right
+            }
+            
+            
+            print("\n\(questionNumber). \(question)")
+            print("Your answer: ", terminator: "")
+            if let inputAnswer = readLine() {
+                guard let intAnswer = Int(inputAnswer) else {
+                    print("Incorrect data type")
+                    continue
+                }
+                questionNumber += 1
+                if answer == intAnswer {
+                    score += 1
+                    print("\nCorrect!")
+                } else {
+                    print("\nSorry, that's wrong.")
+                }
+            }
+        } while questionNumber < 10
+        
+        print("\nYou scored \(score).")
+    }
+}
+
+let game = GameContext()
+game.start()
